4f24a7d54ac80692b814eb507a2ee63986604a1c
udhay <udhayasankar200721@gamil.com>
2025-05-05 18:07:55 +0530

    DB

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,8 +1,8 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL;
-const SUPABASE_PUBLISHABLE_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY;
+const SUPABASE_URL = "https://cawfmtntijvtiemplmff.supabase.co";
+ const SUPABASE_PUBLISHABLE_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNhd2ZtdG50aWp2dGllbXBsbWZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDM2MDcyODQsImV4cCI6MjA1OTE4MzI4NH0.jhPoSziLG0WBQnVwBmdvVanHyccc6l0BkRyIDa17-ac";
 
 
 const supabase = createClient(SUPABASE_URL, SUPABASE_PUBLISHABLE_KEY, {
